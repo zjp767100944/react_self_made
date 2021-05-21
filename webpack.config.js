250eac9ab627347bd870330ac7bdb8879d22c749
张俊鹏 <zhangjunpeng.l1n@bytedance.com>
2021-05-22 01:39:50 +0800

    feat: 启动本地开发服务器

--- webpack.config.js
+++ webpack.config.js
@@ -23,4 +23,15 @@ module.exports = {
       inject: true,
     }),
   ],
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    historyApiFallback: true,
+    overlay: {
+      // 当出现编译器错误或警告时，就在网页上显示一层黑色的背景层和错误信息
+      errors: true,
+    },
+    inline: true,
+    hot: true,
+  },
 };
